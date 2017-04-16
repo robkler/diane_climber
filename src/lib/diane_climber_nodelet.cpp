60c5722303ad428746150183bf913925df802f5a
robkler <robklerjr@poli.ufrj.br>
2017-04-16 13:34:20 -0300

    colocando o id nas funçoes que precisam
    Signed-off-by: robkler <robklerjr@poli.ufrj.br>

--- src/lib/diane_climber_nodelet.cpp
+++ src/lib/diane_climber_nodelet.cpp
@@ -50,7 +50,96 @@ void diane_climber::DianeClimberNodelet::onInit()
 
 }
 
+void diane_climber::DianeClimberNodelet::teste1()//se puder colocar no serviço seria o ideal
+{
+    diane_climber::DianeClimberNodelet::ClimberStair(50 , 0.5);
+}
 
+
+
+bool diane_climber::DianeClimberNodelet::ClimbStairCallback(diane_climber::ClimbStair::Request &req, diane_climber::ClimbStair::Response &res)
+{
+    //Initialize the climbing algorithm
+    //diane_climber::DianeClimberNodelet::ClimberStair(req.stair_angle , 0.5);
+    return true;
+}
+
+
+void diane_climber::DianeClimberNodelet::ClimberStair(const float angle, const float dist)
+{
+
+    controller::Control Msg;
+    int Id;
+    //Parameters
+    float linearSpeed = 0.5;
+    float ArmClimb = 50;
+    float posArmClimb = 0;
+    Id =GetNewControlID();
+
+
+    while (posFrontArm != ArmClimb)//Put the arms in position
+    {
+        Msg = CreateMsgPos(Id ,0 , 0 , ArmClimb , ArmClimb);
+        msgInputControlPub.publish(Msg);
+
+    }
+
+
+    // Go ahead until dont climb stair
+    while(kinectAngle+2 < angle)
+    {
+        Msg = CreateMsgVel(Id, linearSpeed,0,0,0);
+        msgInputControlPub.publish(Msg);
+
+    }
+
+    while(posFrontArm != posArmClimb && posRearArm != posArmClimb)
+    {
+
+        Msg = CreateMsgPos(Id,0.1,0,0,0);
+        msgInputControlPub.publish(Msg);
+
+    }
+
+    Msg = CreateMsgPos(Id,0,0,0,0);
+    msgInputControlPub.publish(Msg);
+
+
+
+}
+
+//------------------------CREATE MESSAGE----------------------------------------------------------------------------------------
+controller::Control diane_climber::DianeClimberNodelet::CreateMsgPos(int Id, float velLin, float velAng , float posArmF, float posArmB )
+{
+    controller::Control Msg;
+    Msg.originId = Id;
+    Msg.modes.push_back(3);
+    Msg.data.push_back(velLin);
+    Msg.data.push_back(velAng);
+    Msg.data.push_back(posArmF);
+    Msg.data.push_back(posArmB);
+    return Msg;
+}
+
+controller::Control diane_climber::DianeClimberNodelet::CreateMsgVel(int Id, float velLin, float velAng, float velArmF, float velArmB)
+{
+    controller::Control Msg;
+    Msg.originId = Id;
+    Msg.modes.push_back(4);
+    Msg.data.push_back(velLin);
+    Msg.data.push_back(velAng);
+    Msg.data.push_back(velArmF);
+    Msg.data.push_back(velArmB);
+    return Msg;
+}
+
+void diane_climber::DianeClimberNodelet::TreatFeedback(std_msgs::Float64MultiArray msg)
+{
+    kinectAngle =  msg.data[1];
+    velLin = msg.data[2];
+    posFrontArm = msg.data[3];
+    posRearArm = msg.data[4];
+}
 unsigned char diane_climber::DianeClimberNodelet::GetNewControlID()
 {
     controller::RequestIDRequest req;
@@ -67,96 +156,6 @@ unsigned char diane_climber::DianeClimberNodelet::GetNewControlID()
     }
 
 }
-
-
-
-void diane_climber::DianeClimberNodelet::TreatFeedback(std_msgs::Float64MultiArray msg)
-{
-    kinectAngle =  msg.data[1];
-    velLin = msg.data[2];
-    posFrontArm = msg.data[3];
-    posRearArm = msg.data[4];
-    cout<<msg.data[4]<<endl;
-}
-
-void diane_climber::DianeClimberNodelet::teste1()
-{
-    diane_climber::DianeClimberNodelet::ClimberStair(50 , 0.5);
-   // msgInputControlPub.publish(MSG);
-}
-
-
-
-bool diane_climber::DianeClimberNodelet::ClimbStairCallback(diane_climber::ClimbStair::Request &req, diane_climber::ClimbStair::Response &res)
-{
-    //Initialize the climbing algorithm
-    //diane_climber::DianeClimberNodelet::ClimberStair(req.stair_angle , 0.5);
-    //msgInputControlPub.publish(MSG);
-    return true;
-}
-
-
-void diane_climber::DianeClimberNodelet::ClimberStair(const float angle, const float dist)
-{
-    if(dist<0.8)
-    {
-        controller::Control Msg;
-
-        float ArmClimb = 50;
-        int Id =1; //TODO ARRUMAR ESSE ID
-        while (posFrontArm != ArmClimb)
-        {
-            Msg = CreateMsgPos(Id ,0 , 0 , ArmClimb , ArmClimb);
-            msgInputControlPub.publish(Msg);
-
-        }
-        //Andar em direçao a escada e ir verificando o acelerometro
-        float linearSpeed = 0.5;
-        CreateMsgVel(Id,linearSpeed , 0 , 0 , 0);
-        usleep(dist*1000/linearSpeed);
-        // Quando estiver em cima da escada subir controladamente e verificar o acelerometro para quando ele começar a abaixar
-        while(kinectAngle+2 < angle)
-        {
-            Msg = CreateMsgVel(Id, linearSpeed,0,0,0);
-        }
-        //abaixar os armF
-        float posArmClimb = 0;
-        while(posFrontArm != posArmClimb && posRearArm != posArmClimb)
-        {
-
-            Msg = CreateMsgPos(Id,0.1,0,0,0);
-        }
-
-        Msg = CreateMsgPos(Id,0,0,0,0);
-
-    }
-}
-
-//------------------------CREATE MESSAGE----------------------------------------------------------------------------------------
-controller::Control diane_climber::DianeClimberNodelet::CreateMsgPos(int Id, float velLin, float velAng , float posArmF, float posArmB )
-{
-    controller::Control Msg;
-    Msg.originId = 1;
-    Msg.modes.push_back(3);
-    Msg.data.push_back(velLin);
-    Msg.data.push_back(velAng);
-    Msg.data.push_back(posArmF);
-    Msg.data.push_back(posArmB);
-    return Msg;
-}
-
-controller::Control diane_climber::DianeClimberNodelet::CreateMsgVel(int Id, float velLin, float velAng, float velArmF, float velArmB)
-{
-    controller::Control Msg;
-    Msg.originId = 1;
-    Msg.modes.push_back(4);
-    Msg.data.push_back(velLin);
-    Msg.data.push_back(velAng);
-    Msg.data.push_back(velArmF);
-    Msg.data.push_back(velArmB);
-    return Msg;
-}
-
 //--------------------------------------------------------------------------------------------------------------------------------
 diane_climber::DianeClimberNodelet::~DianeClimberNodelet()
 {
